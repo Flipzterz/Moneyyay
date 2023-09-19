a2977bec06f28ab8bc21e66db877a4f0ad290076
Spoonz <85467846+Flipzterz@users.noreply.github.com>
2023-09-19 10:04:51 -0400

    Add files via upload

--- Monbey.py
+++ Monbey.py
@@ -1,4 +1,7 @@
-pennies = int(input("Enter the number of pennies: "))
+def null():
+    print("")
+
+pennies = int(input("Enter your desired number of pennies: "))
 
 dollars = pennies // 100
 pennies %= 100
@@ -14,9 +17,12 @@ pennies %= 5
 
 remaining_pennies = pennies
 
-#results
+total = dollars + quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + remaining_pennies * 0.01
+
+# Print results
 print("Dollar Bills =", dollars)
 print("Quarters =", quarters)
 print("Dimes =", dimes)
 print("Nickels =", nickels)
 print("Pennies =", remaining_pennies)
+print("Total Money: $", total)
